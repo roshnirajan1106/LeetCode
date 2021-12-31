9d0f8c517397c91d5ff5b2e1ce01f8906e880100
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2021-12-31 19:48:01 +0530

    Time: 11 ms (30.59%), Space: 13.6 MB (21.53%) - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -4,36 +4,19 @@ public:
         
         if(nums.size() == 1)
             return nums[0];
-        set<int>s (nums.begin(),nums.end());
-        map<int,int> m;
-        for(int i = 0 ;i<nums.size();i++)
+       int n = 10001;
+        vector<int> dp(n,0);
+        vector<int> sum(n,0);
+        for(int i = 0;i<nums.size();i++)
         {
-            if(m.find(nums[i]) == m.end())
-                m[nums[i]] = 1;
-            else{
-                m[nums[i]] ++;
-            }
+            sum[nums[i]] += nums[i];
         }
-        int max_len = 1;
-        int n = s.size();
-        vector<int> dp(n+1,0);
-        vector<int> s1(s.begin(),s.end());
-        for(auto p :s1)
-            cout<<p<<" ";
         dp[0] = 0;
-        dp[1] = s1[0]*m[s1[0]];
-        for(int i =2;i<=n;i++)
-        {
-            int ans = s1[i-1]*m[s1[i-1]];
-            if(s1[i-1] == (s1[i-2] + 1))
-            {
-                ans += dp[i-2];
-            }
-            else{
-                ans += dp[i-1];
-            }
-            dp[i] = max(dp[i-1] , ans);
-        }
-        return dp[n];
+        dp[1]  = sum[1];
+        for(int i =2;i<n;i++)
+            dp[i] = max(dp[i-1], dp[i-2] + sum[i]);
+        
+        return dp[n-1];
+        
     }
     };
