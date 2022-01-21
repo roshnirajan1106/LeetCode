5c0438aa2afcc5b550a6f08bfd23d53981633c65
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-01-21 21:52:57 +0530

    Time: 8 ms (10.37%), Space: 6.6 MB (66.51%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int find_the_distinct_permutations(vector<int>&dp, vector<int>&nums,int sum, int target)
-    {
-        if(sum == target)
-            return 1;
-        if(sum  > target)
-            return 0;
-        if(dp[sum]!= -1 )
-            return dp[sum];
-        int count = 0;
-        for(int i = 0; i<nums.size();i++)
-        {   if(sum + nums[i] <=target)
-                count += find_the_distinct_permutations(dp,nums,sum+nums[i],target);
-        }
-        dp[sum] = count;
-        return dp[sum];
-        
-    }
+   
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target + 1,-1);
-       return  find_the_distinct_permutations(dp, nums, 0,target);
+        vector<long int>dp(target+1,0);
+        dp[0] =1;
+        for(int i = 1;i<=target;i++)
+        {   int x =0;
+            for(int j = 0 ;j<nums.size();j++)
+            {   
+                if(i >= nums[j])
+                    x += dp[i-nums[j]];
+            }
+         dp[i] = x;
+        }
+        int ans = dp[target];
+        return ans;
     }
 };
