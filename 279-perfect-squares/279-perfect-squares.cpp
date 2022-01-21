de6ef0311eb8a2ea9051ba1bca1bec3ab436ba7b
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-01-21 22:55:53 +0530

    Time: 107 ms (74.34%), Space: 9.2 MB (45.04%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSquares(int n) {
+       if(n == 1)
+           return 1;
+        vector<int>dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++)
+        {   int ans =INT_MAX;
+            for(int j = 1;j*j<=i;j++)
+            {
+                ans = min(ans, 1 + dp[i-j*j]);
+            }
+         dp[i]=ans;
+        }
+        return dp[n];
+    }
+};
