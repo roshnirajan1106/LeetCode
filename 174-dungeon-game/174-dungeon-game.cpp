d73de0da238ddded69c1555d7e749dea0af09f22
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-01-28 13:27:10 +0530

    Time: 4 ms (95.46%), Space: 8.9 MB (69.32%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    int dun(vector<vector<int>>&dp, int i ,int j, int m ,int n,vector<vector<int>>& dungeon)
-    {
-     if(i > m-1 || j >n-1)
-         return INT_MAX;
-        if(i == m-1 && j== n-1)
-        {
-            return (dungeon[i][j] <=0 ? -dungeon[i][j] +1 :1);
-        }
-        if(dp[i][j] != INT_MAX)
-            return dp[i][j];
+//     int dun(vector<vector<int>>&dp, int i ,int j, int m ,int n,vector<vector<int>>& dungeon)
+//     {
+//      if(i > m-1 || j >n-1)
+//          return INT_MAX;
+//         if(i == m-1 && j== n-1)
+//         {
+//             return (dungeon[i][j] <=0 ? -dungeon[i][j] +1 :1);
+//         }
+//         if(dp[i][j] != INT_MAX)
+//             return dp[i][j];
         
-        int down = dun(dp,i+1,j, m,n, dungeon);
-        int right = dun(dp,i,j+1,m,n, dungeon);
-        int ans = min(down, right) - dungeon[i][j];
-        dp[i][j] = (ans <=0 ? 1 : ans);
-        return dp[i][j];
-    }
+//         int down = dun(dp,i+1,j, m,n, dungeon);
+//         int right = dun(dp,i,j+1,m,n, dungeon);
+//         int ans = min(down, right) - dungeon[i][j];
+//         dp[i][j] = (ans <=0 ? 1 : ans);
+//         return dp[i][j];
+//     }
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int m = dungeon.size();
         int n = dungeon[0].size();
        vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
-        return dun(dp, 0,0,m,n,dungeon);
+        for(int i = m-1 ;i>=0;i--)
+        {
+            for(int j = n-1;j>=0;j--)
+            {
+                if(i == m-1 && j == n-1)
+                {
+                    dp[i][j] = (dungeon[i][j] <=0 ? -dungeon[i][j] +1 :1);
+                }
+                else{
+                    int down = INT_MAX, right =INT_MAX;
+                    if(i < m-1)
+                    {
+                        down = dp[i+1][j];
+                    }
+                    if(j < n-1)
+                        right = dp[i][j+1];
+                    
+                    int ans = min(down,right) - dungeon[i][j] ;
+                    dp[i][j] = ans <=0 ? 1 : ans;
+                }
+            }
+        }
+        return dp[0][0];
+       
     }
 };
