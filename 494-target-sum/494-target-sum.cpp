01f097924869f78dffe11e36274dd8c779bd595c
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-01-30 11:55:06 +0530

    Time: 11 ms (80.21%), Space: 9.4 MB (49.18%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,52 +1,30 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        vector<int> num;
-        int count = 0;
-        int total_sum =0;
+        int sum = 0;
+        int zeros = 0;
         for(int i = 0;i<nums.size();i++)
         {
-            if(nums[i] != 0)
-            {
-                num.push_back(nums[i]);
-                total_sum += nums[i];
-            }
-            else{
-                count++;
-            }
+            sum += nums[i];
+            if(nums[i] == 0)
+                zeros++;
         }
-        int sum = (target+total_sum);
-        if(sum <0)
+        sum += target;
+        if(sum < 0)
             return 0;
-        if(sum % 2 !=0)
+        if(sum % 2 != 0)
             return 0;
-        
         sum = sum /2;
-        //sum =  4 -> 4-1 , 4+1 = 5;
-        // totalsum = 10 , target = 4 => 7-3 is an option
-        //6-2 is an option but 6 + 2 is not 10 , but we need 10
-        int n = num.size();
-        int dp[n+1][sum+1];
-        memset( dp, 0, sizeof(dp) );
-        for(int i = 0 ;i<n+1;i++)
+        int n = nums.size();
+        vector<int>dp(sum+1,0);
+        dp[0] = 1;
+        for(auto num : nums)
         {
-            dp[i][0] = 1;
-        }
-        for(int i = 1;i<n+1;i++)
-        {
-            for(int j =1;j<sum+1;j++)
+            for(int s= sum; s>=num;s--)
             {
-                if( num[i-1] <= j)
-                {
-                    dp[i][j] += dp[i-1][j-num[i-1]];
-                }
-                    dp[i][j] += dp[i-1][j];
-                
-                cout<<dp[i][j]<<" ";
+                dp[s] += dp[s-num];
             }
-            cout<<endl;
         }
-        return dp[n][sum]*(1<<count);
-        
+        return dp[sum];
     }
 };
