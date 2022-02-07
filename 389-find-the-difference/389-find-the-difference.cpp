bcf7596a03ec5a25223a3401e780a1a479a162a6
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-02-07 09:25:58 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char,int>m;
-        char c;
-        for(int i = 0;i<s.length();i++)
-        {
-            m[s[i]]++;
-        }
-        for(int i = 0;i<t.length();i++)
-        {
-            if(m.find(t[i]) == m.end() || m[t[i]] == 0)
-            {
-                c = t[i];
-                break;
-            }
-            else
-                m[t[i]] --;
-        }
-        return c;
+        char r = 0;
+        for(char c :s)
+            r= r ^ c;
+        for(char c : t)
+            r= r^ c;
+        
+        return r;
+        
     } 
 };
