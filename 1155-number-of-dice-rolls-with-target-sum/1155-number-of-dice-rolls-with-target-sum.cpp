d3f6c8d5d1bc2d31004c662b05d8a8966236848f
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-02-14 14:11:39 +0530

    Time: 52 ms (42.88%), Space: 8.4 MB (54.27%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     int mod = 1000000007;
-    int count(int n, int target, int f , vector<vector<int>>&dp)
+    int function(int n, int k, int t,vector<vector<int>> &dp)
     {
-        if(target == 0)
-            return 1;
-        if(n == 0)
-            return 0 ;
-        if(dp[n][target])
-            return dp[n][target] - 1;
+        if(t == 0 && n == 0)
+        {
+                return 1;
+            
+        }
+        if( n==  0)
+            return 0;
+        if(t <=0)
+            return 0;
         
-        for(int i = 1;i<=f;i++)
+        if(dp[n][t] != -1)
+            return dp[n][t];
+            
+        int ans =0;
+        for(int i =1;i<=k;i++)
         {
-            if(i <=target)
-                dp[n][target] =(dp[n][target] + count(n-1,target-i,f,dp)) % mod;
+            ans = (ans +  function(n-1,k, t - i,dp)) %mod;
+            
         }
-        dp[n][target] +=1;
-        return dp[n][target];
+        dp[n][t] = ans;
+        return dp[n][t];
     }
+    
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<long int>>dp(n+1 , vector<long int >(target+1,0));
-        dp[0][0] =1 ;
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j = 1; j<=target;j++)
-            {
-                for(int m = 1;m<=k && m<=j;m++)
-                {
-                    dp[i][j] = (dp[i][j] + dp[i-1][j-m] % mod) %mod;
-                }
-            }
-        }
-        return dp[n][target];
+        int cnt = 0;
+        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+         cnt = function(n, k, target,dp);
+        return cnt;
     }
 };
