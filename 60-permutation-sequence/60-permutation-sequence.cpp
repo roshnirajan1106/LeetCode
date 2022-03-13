69bc1c2dbf7bab53538f080e951d2bdf16fbd289
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-03-13 19:16:01 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.04%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    void permutation(string s, int i,vector<string>&res)
-    {
-        if(i == s.length() )
-        {
-            res.push_back(s);
-            return ;
-        }
-        for(int j = i;j<s.length();j++)
-        {
-            swap(s[i],s[j]);
-            permutation(s,i+1,res);
-            swap(s[i],s[j]);
-        }
-            
-    }
     string getPermutation(int n, int k) {
-        string s = "";
-        for(int i =1;i<=n;i++)
-        {
-            s += i +'0';
-        }
-        vector<string>res;
-        
-        k--;
         int fact = 1;
+        vector<int>nums;
         for(int i =1;i<n;i++)
-            fact =fact*i;
-        if(k >= fact)
         {
-            int rem = k /fact;
-            swap(s[rem],s[0]);
-            k = k-fact*rem;
+            nums.push_back(i);
+            fact = fact *i;
         }
-        permutation(s,1,res);
-        sort(res.begin(),res.end());
-        
-        return res[k];
+        nums.push_back(n);
+        string s = "";
+        k--;
+        while(true)
+        {
+            char str = nums[k/fact] + '0';
+            s = s + str;
+            nums.erase(nums.begin() + k/fact);
+            if(nums.size() == 0)
+                break;
+            
+            k = k%fact;
+            fact= fact/nums.size();
+        }
+        return s;
     }
 };
