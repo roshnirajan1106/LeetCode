c37d3e45ba7754bd0e8eee37e37503f098bf56c3
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-03-17 10:41:19 +0530

    Time: 10 ms (62.49%), Space: 10.8 MB (71.91%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    void dfs(vector<int>ans,vector<vector<int>>&res,int i,int n,int sum ,int target,vector<int>& candidates)
-    {
+    void find(vector<vector<int>>&ans,vector<int>&res,int i, int n ,vector<int>&candidates,int &sum,int &target){
         if(i == n)
-            return ;
-        if(sum > target)
-            return ;
-        if(sum == target)
         {
-            res.push_back(ans);
             return ;
         }
-        //include
-        ans.push_back(candidates[i]);
-        dfs(ans,res,i,n,sum +candidates[i],target,candidates);
-        ans.pop_back();
-        dfs(ans,res,i+1,n,sum,target,candidates);
+        if(sum == target)
+        {
+            ans.push_back(res);
+            return ;
+        }
+        if(sum > target)
+            return ;
         
+        res.push_back(candidates[i]);
+        sum = sum+candidates[i];
+        find(ans,res,i,n,candidates,sum,target);
+        sum = sum -candidates[i];
+        res.pop_back();
+        find(ans,res,i+1,n,candidates,sum,target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>res;
         vector<int>ans;
+        int i = 0;
         int n = candidates.size();
-        dfs(ans,res,0,n,0,target,candidates);
+        int sum = 0;
+        find(res,ans,i,n,candidates,sum,target);
         return res;
     }
 };
