b21abf90c25a7be598aa9be20363f262d32a9eb6
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-03-21 10:28:19 +0530

    Time: 10 ms (21.93%), Space: 6.9 MB (13.71%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int>nums(26,0);
-        for(int i = 0; i<s.length();i++)
-        {
-            int n = s[i]-'a';
-            nums[n] = i;
-        }
-        for(auto p :nums)
-            cout<<p<<" ";
-        int i = 0;
+        int start = 0;
+        int end =0;
+        int i =0;
         vector<int>res;
-        int n = s.length();
-        while(i < n)
-        {   
-            int num =  s[i] -'a';
-            int max_i = nums[num];
-            int j = i+1;
-            while(j < max_i)
+        map<char,int>m;
+        for(int i = 0;i<s.length();i++)
+            m[s[i]] = i;
+            
+        while(i < s.length())
+        {
+            end = max(end,m[s[i]]);
+            if(i == end)
             {
-                 num = s[j]-'a';
-                max_i = max(max_i,nums[num]);
-                j++;
+                int size = end- start + 1;
+                res.push_back(size);
+                start = i+1;
                 
             }
-            res.push_back(max_i-i+1);
-            i = max_i + 1;
-            
+            i++;
         }
         return res;
-        
     }
 };
