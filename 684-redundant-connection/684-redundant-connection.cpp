e01188be9e44d15124fb98d9000674549e47710f
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-03-29 10:44:19 +0530

    Time: 14 ms (39.39%), Space: 8.8 MB (72.63%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,60 +1,49 @@
 class Solution {
 public:
-    int find(int x , vector<int> &parent)
-    {   
-        if(parent[x] == -1)
-            return x;
-     
-        parent[x] = find(parent[x], parent);
-        return parent[x];
-        // return 1;
+    int find(int i, vector<int>&parent)
+    {
+        if(parent[i] == -1)
+            return i;
+        return parent[i] = find(parent[i],parent);
     }
-    void union_set(int x, int y, vector<int> &parent, vector<int> &rank)
-    {  
-        int s1 = find(x, parent);
-        int s2 = find(y,parent);
+    bool unionset(int i,int j, vector<int>&parent, vector<int>&rank)
+    {
+        int s1 = find(i,parent);
+        int s2 = find(j,parent);
+        if(s1 == s2)
+            return true;
         
-        if(s1 != s2)
+        
+        if(rank[s1] > rank[s2])
         {
-             if(rank[s1] >  rank[s2])
-            {
-                parent[s2] = s1;
-                rank[s1] += rank[s2];
-            }else{
-                parent[s1] = s2;
-                rank[s2] += rank[s1];
-            }
-           
+            parent[s2] = s1;
+            rank[s1] += rank[s2];
         }
-           
+        else
+        {
+            parent[s1] = s2;
+            rank[s2] += rank[s1];
+        }
+        return false;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n= edges.size();
-       vector<int> rank(n,1);
-        vector<int> parent(n,-1);
-        vector<int> res;
-        for(auto p : edges)
-        {
-            int x= p[0]-1;
-            int y = p[1]-1;
+        int n = edges.size();
+        vector<int>parent(n,-1);
+        vector<int>rank(n,-1);
+        vector<int>ans;
+        for(int i = 0;i<n ;i++){
+           
+                int src = edges[i][0]-1;
+                int dest = edges[i][1]-1;
+                bool res =unionset(src,dest,parent,rank);
+                if(res == true){
+                    ans.push_back(src+1);
+                    ans.push_back(dest+1);
+                    return ans;
+                }
             
-            int s1 =  find(x,parent);
-            int s2 = find(y,parent);
-            cout<<s1<<" "<<s2<<endl;
-            if(s1 == s2)
-            {
-                res.push_back(x+1);
-                res.push_back(y+1);
-                break;
-            }
-            else{
-                union_set(x,y,parent, rank);
-                
-            }
-
-        
         }
-        return res;
-    
+        return ans;
+        
     }
 };
