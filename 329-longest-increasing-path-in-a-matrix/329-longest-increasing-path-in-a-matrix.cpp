ef666e64f4bda596754255e54914a34e6c5f651a
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-03-30 17:10:15 +0530

    Time: 44 ms (89.25%), Space: 17 MB (35.01%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    int dfs(int i,int j ,int m, int n, vector<vector<int>>& visited,vector<vector<int>>& dp, vector<vector<int>>& matrix,int p)
+    int dfs(int i , int j,int m,int n,vector<vector<int>>&visited,vector<vector<int>>&matrix,int prev,vector<vector<int>>&dp)
     {
-        if(i > m-1 || j > n-1 || i<0 || j<0)
+        if(i > m -1 || j >n -1 || i <0 || j <0)
             return 0;
         if(visited[i][j] == 1)
             return 0;
-        if(p != -1)
-        {
-            if(p >= matrix[i][j])
-                return 0;
-        }
+        if(prev >= matrix[i][j])
+            return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
-        
         visited[i][j] = 1;
-        int a = dfs(i+1,j, m,n, visited, dp, matrix, matrix[i][j]);
-        int b = dfs(i,j+1, m,n, visited, dp, matrix, matrix[i][j]);
-        int c = dfs(i,j-1, m,n, visited, dp, matrix, matrix[i][j]);
-        int d = dfs(i-1,j, m,n, visited, dp, matrix, matrix[i][j]);
+         prev = matrix[i][j];
+        int a = dfs(i+1,j, m,n,visited,matrix,prev,dp);
+        int b = dfs(i-1,j, m,n,visited,matrix,prev,dp);
+        int c = dfs(i,j+1, m,n,visited,matrix,prev,dp);
+        int d = dfs(i,j-1, m,n,visited,matrix,prev,dp);
+        
         visited[i][j] = 0;
-        dp[i][j] =max(a,max(b,max(c,d))) + 1;
-        return dp[i][j];
+        return dp[i][j] = max(a,max(b,max(c,d)))  + 1;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> visited(m,vector<int>(n,0));
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        int ans= 0;
-        for(int i = 0 ;i < m ;i++)
+        int prev = -1;
+        int ans = 0;
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        for(int i = 0;i<m;i++)
         {
             for(int j = 0;j<n;j++)
             {
-              ans = max(ans, dfs(i,j, m,n,visited, dp, matrix,-1)) ;
+                ans = max(ans, dfs(i,j,m,n,visited,matrix,prev,dp));
+                
             }
         }
-         return ans;
+        return ans;
     }
 };
