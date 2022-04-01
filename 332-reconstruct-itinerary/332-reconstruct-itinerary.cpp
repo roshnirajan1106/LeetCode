632528b01e36a8379a250ce7be31c0408b522261
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-04-01 11:16:33 +0530

    Time: 33 ms (51.10%), Space: 15.3 MB (35.52%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-   void dfs(string src, vector<string>&output, map<string,multiset<string>>&graph)
-   {
-       while(graph[src].size())
-       {   
-           string p = *graph[src].begin();
-           graph[src].erase(graph[src].begin());
-           dfs(p, output, graph);
-       }
-       output.push_back(src);
-   }
+    
+    void dfs(string src, vector<string>&output,  map<string,multiset<string>>&graph)
+    {
+        while(graph[src].size() > 0)
+        {
+            string x = *graph[src].begin();
+            graph[src].erase(graph[src].begin());
+            
+            dfs(x,output,graph);
+        }
+        output.push_back(src);
+    }
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-         map<string,multiset<string>> graph;
+        map<string,multiset<string>>graph;
         for(auto p : tickets)
         {
-            string src = p[0];
+            string src =p[0];
             string dest = p[1];
             graph[src].insert(dest);
         }
-        vector<string>output;
-        string src = "JFK";
         
-         dfs(src, output, graph);
-        reverse(output.begin(), output.end());
+        string src ="JFK";
+        vector<string>output;
+        dfs(src, output, graph);
+        reverse(output.begin(),output.end());
         return output;
         
     }
