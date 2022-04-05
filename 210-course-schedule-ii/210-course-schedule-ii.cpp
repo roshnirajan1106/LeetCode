9b7431f9bc714c5776aa98b276f5685705d0cf39
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-04-05 13:20:40 +0530

    Time: 52 ms (14.35%), Space: 15 MB (21.38%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,54 +1,71 @@
 class Solution {
 public:
-    bool dfs(vector<int>&ans,  map<int, vector<int>> &graphs,vector<int> &visited, vector<int> stack , int src )
+    vector<int> dfs(int src,map<int,vector<int>>&graph,vector<int>&visited,vector<int>&stack,vector<int>&ans)
     {
-        visited[src] =1;
-        stack[src]  =1;
-        for(auto p : graphs[src])
+        stack[src] = 1;
+        visited[src]  =1;
+        for(auto p: graph[src])
         {
             if(stack[p] == 1)
-                return false;
-            if(visited[p] == 0)
             {
-                bool flag  = dfs(ans, graphs, visited, stack, p);
-                if(flag == false)
-                    return false;
+                vector<int>v;
+                return v;
             }
+            if(visited[p] == 1)
+                continue;
+            vector<int>v  = dfs(p,graph,visited,stack,ans);
+            if(v.size() == 0)
+                return v;
+            
         }
         ans.push_back(src);
-        stack[src] = 0;
-        return true;
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> ans;
-        vector<int> visited(numCourses,0);
-        vector<int> stack(numCourses,0);
-        map<int, vector<int>> graphs;
-        vector<int> v;
-        for(int i = 0;i<prerequisites.size();i++)
-        {
-            int x = prerequisites[i][0];
-            int y =prerequisites[i][1];
-            
-            graphs[x].push_back(y);
-        
-        }
-        
-        for(int i=0;i<numCourses;i++)
-        {
-            if(visited[i] == 0)
-            {
-              bool flag=   dfs(ans, graphs, visited, stack, i);
-                if(flag == false)
-                {
-                    
-                    return v;
-                }
-                
-            }
-        }
-        
         return ans;
         
+    }
+    vector<int> bfs(map<int,vector<int>>&graph,vector<int>&indegree,int num)
+    {
+        queue<int>q;
+        for(int i = 0;i<num;i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty())
+        {
+            auto top =q.front();
+            q.pop();
+            ans.push_back(top);
+            
+            for(auto p : graph[top])
+            {
+                if(--indegree[p] == 0 )
+                    q.push(p);
+            }
+            
+        }
+        return ans;
+        
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        map<int,vector<int>>graph;
+        vector<int>indegree(numCourses,0);
+        for(auto p :prerequisites)
+        {
+            int src= p[0];
+            int dest = p[1];
+            indegree[src]++;
+            graph[dest].push_back(src);
+        }
+        vector<int>v;
+        vector<int>visited(numCourses,0);
+        vector<int>stack(numCourses,0);
+        
+        vector<int>ans1 = bfs(graph,indegree,numCourses);
+        if(ans1.size() == numCourses)
+            return ans1;
+        return v;
     }
 };
