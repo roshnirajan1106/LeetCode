77bee667eca887751b471cf87b65d2bdfb10d460
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-04-30 11:43:21 +0530

    Time: 6 ms (26.57%), Space: 8.7 MB (10.24%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,68 +1,68 @@
 class Solution {
 public:
-    double bfs(string src, string dest,map<string,vector<pair<string,double>>>&graph,map<string,int>visited){
-        queue<pair<string,double>>q;
-        q.push({src,1});
+    double bfs(string src,string dest,map<string,vector<pair<string,double>>>&graph)
+    {
         cout<<src<<" "<<dest<<endl;
+        double ans = 1.0;
+        queue<pair<string,double>>q;
+        int n = graph.size();
+        map<string,int>visited;
+        for(auto p : graph)
+            visited[p.first] = 0;
+        q.push({src,ans});
         while(!q.empty())
         {
-            int size = q.size();
-            while(size--)
+            auto top = q.front();
+            q.pop();
+            string c = top.first;
+            visited[c] = 1;
+            double val =top.second;
+            cout<<c<<" "<<val<<endl;
+            
+            if(c == dest)
+                return val;
+            for(auto p : graph[c])
             {
-                auto top = q.front();
-                q.pop();
-                
-                string s = top.first;
-                visited[s] = 1;
-                double div = top.second;
-                if(s == dest)
+                if(visited[p.first] == 0)
                 {
-                    return div;
+                     double res = val*p.second;
+                    q.push({p.first,res});
                 }
+               
                 
-                for(auto p :graph[s])
-                {
-                    if(visited[p.first] == 0)
-                        q.push({p.first,p.second*div});
-                        
-                }
             }
         }
-        return -1;
+            return -1;
         
     }
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         map<string,vector<pair<string,double>>>graph;
-        map<string,int>visited;
-        for(int i = 0;i<equations.size();i++)
+        int n = values.size();
+        for(int i = 0;i<n;i++)
         {
-            string s = equations[i][0];
-            string d = equations[i][1];
-            graph[s].push_back({d,values[i]});
-            graph[d].push_back({s,1/values[i]});
-            visited[s] = 0;
-            visited[d] = 0;
+            string src = equations[i][0];
+            string dest = equations[i][1];
+            double val = values[i];
+            graph[src].push_back({dest,val});
+            double x = 1.0/val;
+            graph[dest].push_back({src,x});
         }
         vector<double>res;
         for(auto p : queries)
         {
-            string s = p[0];
-            string d = p[1];
-            double ans ;
-            if(visited.find(s) == visited.end() || visited.find(d) == visited.end())
+            string src = p[0];
+            string dest = p[1];
+            if(graph.find(src) == graph.end())
+                res.push_back(-1);
+            else if(src == dest)
+                res.push_back(1);
+            else
             {
-                ans = -1;
+                double m = bfs(src,dest,graph);
+                res.push_back(m);
             }
             
-            else if(s == d)
-                ans = 1;
-            else
-                ans = bfs(s,d,graph,visited);
-            
-            res.push_back(ans);
-                
         }
         return res;
-        
     }
 };
