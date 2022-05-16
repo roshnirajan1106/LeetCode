e4f9260168a9050688691a6399373e962cf5bdde
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-05-16 15:05:05 +0530

    Time: 68 ms (82.31%), Space: 21 MB (45.51%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,59 +1,57 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
+        queue<pair<int,int>>q;
+        if(grid[0][0] == 1)
             return -1;
-        int dist = 0;
-        queue<pair<int,int>> q;
-        
-        
-        
-       
-        
-        vector<vector<int>> v;
-        v.push_back({0,1});
-        v.push_back({0,-1});
-        
-        v.push_back({1,0});
-        v.push_back({-1,0});
-        v.push_back({1,1});
-        v.push_back({1,-1});
-        v.push_back({-1,1});
-        v.push_back({-1,-1});
-        
-        
-        grid[0][0]=1;
+        int m = grid.size();
+        int n = grid[0].size();
+    vector<vector<int>>visited(m,vector<int>(n,0));
+
         q.push({0,0});
-      
+        
+        visited[0][0] = 1;
+        
+        int len = 1;
+        vector<pair<int,int>>arr;
+        arr.push_back({0,1});
+        arr.push_back({0,-1});
+        arr.push_back({1,0});
+        arr.push_back({-1,0});
+        arr.push_back({-1,-1});
+        arr.push_back({1,1});
+        arr.push_back({-1,1});
+        arr.push_back({1,-1});
         while(!q.empty())
         {
-            int size=  q.size();
-          
-            dist++;
+            int size = q.size();
             while(size--)
-            {  
-                pair<int,int> top = q.front();
+            {
+                auto top = q.front();
                 q.pop();
                 int x = top.first;
                 int y = top.second;
-                cout<<x<<" "<<y<<endl;
-                if(x == n-1 && y == n-1)
-                    return dist;
-                for(int i = 0 ;i<8;i++)
+                //visited[x][y] = 1;
+                if(x == m-1 && y == n-1)
+                    return len;
+                
+                
+                for(auto p : arr)
                 {
-                    int xx = x + v[i][0];
-                    int yy = y + v[i][1];
-                    if(xx > n-1 || xx < 0 || yy > n-1 || yy < 0 || grid[xx][yy] != 0)
+                    int xx = p.first + x;
+                    int yy = p.second + y;
+                    if(xx <0 || yy<0 ||xx >m-1 ||yy>n-1 || visited[xx][yy] ==1 || grid[xx][yy] == 1)
                         continue;
-                    else{
-                        grid[xx][yy] = 1;
-                        q.push({xx,yy});
-                        
-                    }
+                    visited[xx][yy] = 1;
+                    q.push({xx,yy});
+                    
                     
                 }
+                
+                
+                
             }
+            len ++;
             
         }
         return -1;
