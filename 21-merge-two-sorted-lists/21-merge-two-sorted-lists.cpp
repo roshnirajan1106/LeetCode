daba721cbd7528c1073a4420b33f0754f897d4cc
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-07-03 11:36:20 +0530

    Time: 24 ms (6.37%), Space: 23 MB (7.93%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,72 +10,26 @@
  */
 class Solution {
 public:
+    ListNode* merger(ListNode*l1 , ListNode*l2)
+    {
+        if(l1 == NULL)
+            return l2;
+        if(l2 == NULL)
+            return l1;
+        ListNode*n = new ListNode();
+        if(l1->val <= l2->val)
+        {
+            n->val = l1->val;
+            n->next = merger(l1->next,l2);
+        }
+        else if(l1 ->val > l2->val)
+        {
+            n->val = l2->val;
+            n->next = merger(l1,l2->next);
+        }
+        return n;
+    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode*l =NULL;
-        ListNode*prev =NULL;
-        while(list1 != NULL && list2 != NULL)
-        {
-            if(list1->val < list2->val)
-            {
-                if(l == NULL)
-                {
-                    l = new ListNode(list1->val);
-                    prev = l;
-                }
-                    
-                else{
-                    ListNode* node = new ListNode(list1->val);
-                    prev->next= node;
-                    prev = node;
-                }
-                list1 = list1->next;
-                    
-            }
-            else{
-                if(l == NULL)
-                {
-                    l = new ListNode(list2->val);
-                    prev = l;
-                }
-                    
-                else{
-                    ListNode* node = new ListNode(list2->val);
-                    prev->next= node;
-                    prev = node;
-                }
-                  list2 = list2->next;
-            }
-        }
-        while(list1)
-        {
-            if(l == NULL)
-                {
-                    l = new ListNode(list1->val);
-                    prev = l;
-                }
-                    
-                else{
-                    ListNode* node = new ListNode(list1->val);
-                    prev->next= node;
-                    prev = node;
-                }
-              list1 = list1->next;
-        }
-        while(list2)
-        {
-                 if(l == NULL)
-                {
-                    l = new ListNode(list2->val);
-                    prev = l;
-                }
-                    
-                else{
-                    ListNode* node = new ListNode(list2->val);
-                    prev->next= node;
-                    prev = node;
-                }
-            list2 = list2->next;
-        }
-        return l;
+        return merger(list1,list2);
     }
 };
