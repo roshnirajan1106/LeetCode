9c8df075e2fa8c0f35681e932383e2c27d1f2a1e
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-07-14 19:12:41 +0530

    Time: 17 ms (32.85%), Space: 9.1 MB (46.05%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
-    int find(int i, vector<int>&parent)
+    int find(int i,vector<int>&parent)
     {
         if(parent[i] == -1)
             return i;
         return parent[i] = find(parent[i],parent);
     }
-    bool unionset(int i,int j, vector<int>&parent, vector<int>&rank)
+    void unionset(int i , int j, vector<int>&parent,vector<int>&rank)
     {
         int s1 = find(i,parent);
         int s2 = find(j,parent);
-        if(s1 == s2)
-            return true;
-        
-        
         if(rank[s1] > rank[s2])
         {
             parent[s2] = s1;
             rank[s1] += rank[s2];
         }
-        else
-        {
+        else{
             parent[s1] = s2;
-            rank[s2] += rank[s1];
+            rank[s1] += rank[s2];
         }
-        return false;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int>parent(n,-1);
-        vector<int>rank(n,-1);
-        vector<int>ans;
-        for(int i = 0;i<n ;i++){
-           
-                int src = edges[i][0]-1;
-                int dest = edges[i][1]-1;
-                bool res =unionset(src,dest,parent,rank);
-                if(res == true){
-                    ans.push_back(src+1);
-                    ans.push_back(dest+1);
-                    return ans;
-                }
-            
+        vector<int>parent(n+1,-1);
+        vector<int>res;
+        vector<int>rank(n+1,1);
+        for(auto p : edges)
+        {
+            int i = p[0];
+            int j = p[1];
+            int s1 = find(i,parent);
+            int s2 = find(j,parent);
+            if(s1 == s2)
+            {
+                res.push_back(i);
+                res.push_back(j);
+                break;
+            }
+            unionset(i,j,parent,rank);
         }
-        return ans;
-        
-    }
+    return res;
+}
+    
 };
