94545616b09ab852dce551fb74c63605976b3cae
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-08-12 20:46:27 +0530

    Time: 7 ms (91.56%), Space: 14.3 MB (5.57%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,55 +1,50 @@
 class Solution {
 public:
+    int find(int target,vector<int>nums)
+    {
+        int mid = nums.size();
+        int c = 0;
+        int s= 0;
+        int e = mid-1;
+        int ans  = nums.size();
+        while(s<=e)
+        {
+            int mid  = s + (e-s)/2;
+            
+            if(target<= nums[mid])
+            {
+
+                ans = mid;
+                e = mid-1;
+            }
+                
+            else
+                s = mid + 1;
+        }
+        return ans;
+        
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int mid = -1;
-        int s = 0;
-        int e = nums.size()-1;
+        vector<int>ans;
         if(nums.size() == 0)
         {
-            vector<int>ans;
             ans.push_back(-1);
             ans.push_back(-1);
             return ans;
         }
-        int c = -1;
-        while(s <=e )
+        int s = find(target,nums);
+        int e = find(target+1,nums);
+        if(s == nums.size() || nums[s] != target)
         {
-        
-             mid  = s + (e-s)/2;
-            //cout<<mid<<endl;
-            if(nums[mid] == target)
-            {
-              //  cout<<mid<<endl;
-                c = 1;
-                break;
-                    
-            }
-            else if(nums[mid] < target)
-            {
-                s = mid + 1;
-            }
-            else{
-                e = mid - 1;
-            }
-            
-        }
-        if(c == -1)
-        {
-            vector<int>ans;
             ans.push_back(-1);
             ans.push_back(-1);
             return ans;
         }
-        cout<<"heyyyy";
-        cout<<mid<<endl;
-        vector<int>ans;
-         s = mid;
-        while(s > 0 && nums[s-1] == nums[s] )s--;
-         e = mid;
-        while(e < nums.size()-1 && nums[e] == nums[e+1])e++;
         ans.push_back(s);
-        ans.push_back(e);
-        return ans;
-    
+            ans.push_back(e-1);
+            return ans;
+        
+        
+        
     }
 };
