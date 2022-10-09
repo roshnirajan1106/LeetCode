1e3ceefba72f1b3ffbefd8bd1c4a7d4de2e07a13
roshnirajan1106 <56948080+roshnirajan1106@users.noreply.github.com>
2022-10-09 19:40:23 +0530

    Time: 480 ms (47.52%), Space: 16.3 MB (48.63%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -3,34 +3,29 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int close = nums[0] + nums[1] + nums[2];
-        for(int i = 0;i<nums.size();i++)
+        for(int i= 0 ;i<nums.size();i++)
         {
-            
             int sum = 0;
             int lo = i+1;
             int hi = nums.size()-1;
             while(lo < hi)
             {
-                
-                sum =nums[i] +  nums[lo] + nums[hi];
-                
-                if(abs(target-sum) < abs(target-close))
+                sum = nums[i] + nums[lo] + nums[hi];
+                if(abs(target-close) > abs(target- sum))
                 {
                     close = sum;
                 }
-                if(sum == target)
+                if(target == sum)
                 {
-                    while(lo < hi && nums[lo] == nums[lo+1])lo++;
-                    while(lo<hi && nums[hi] == nums[hi-1]) hi--;
-                    lo++;
-                    hi--;
+                    return target;
                 }
-                if(sum < target)
+                else if(sum > target)
                 {
-                    lo++;
+                   hi--;
                 }
-                if(sum > target)
-                    hi--;
+                else
+                    lo++;
+                
             }
         }
         return close;
